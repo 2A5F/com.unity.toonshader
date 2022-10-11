5a5f9b361e24cd0a0c388f674154ed8b2949707b
toshiyuki mori <toshiyuki@unity3d.com>
2022-10-11 19:17:24 +0900

    https://unity.slack.com/archives/CAUGBHMCJ/p1665483339882829?thread_ts=1665446400.431889&cid=CAUGBHMCJ [skip ci]

---
+++ Toonshader_ProjectOutlineHDRP/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using Unity.EditorCoroutines;
+#endif
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
